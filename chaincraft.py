89fb20b870ec73806ad9755aa051c5d69eb7ad7e
José I. O. <30682875+jio-gl@users.noreply.github.com>
2024-08-27 12:10:55 -0700

    Update chaincraft.py

--- chaincraft.py
+++ chaincraft.py
@@ -5,11 +5,10 @@ import threading
 import time
 import zlib
 import hashlib
+import dbm
+import os
 from dataclasses import dataclass, asdict
-from typing import List, Dict
-
-# Persistent Python Key-value
-# https://remusao.github.io/posts/python-dbm-module.html
+from typing import List, Tuple
 
 @dataclass
 class SharedObject:
@@ -17,19 +16,24 @@ class SharedObject:
     timestamp: float
 
     def to_json(self):
-        return json.dumps(asdict(self), sort_keys = True)
+        return json.dumps(asdict(self))
 
     @classmethod
     def from_json(cls, json_str):
         return cls(**json.loads(json_str))
 
 class ChaincraftNode:
-    def __init__(self, max_peers=2):
+    def __init__(self, max_peers=2, reset_db=False):
         self.max_peers = max_peers
-        self.peers: List[tuple] = []
-        self.shared_objects: Dict[str, SharedObject] = {}
+        self.peers: List[Tuple[str, int]] = []
         self.port = random.randint(5000, 9000)
         self.host = '127.0.0.1'
+        self.db_name = f"node_{self.port}.db"
+        
+        if reset_db and os.path.exists(self.db_name):
+            os.remove(self.db_name)
+        
+        self.db = dbm.open(self.db_name, 'c')
 
     def start(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -64,14 +68,14 @@ class ChaincraftNode:
         while True:
             compressed_data, addr = self.socket.recvfrom(1024)
             message_hash = self.hash_message(compressed_data)
-            if message_hash not in self.shared_objects:
+            if message_hash not in self.db:
                 message = self.decompress_message(compressed_data)
                 self.handle_message(message, message_hash, addr)
 
     def handle_message(self, message, message_hash, addr):
         try:
             shared_object = SharedObject.from_json(message)
-            self.shared_objects[message_hash] = shared_object
+            self.db[message_hash] = message
             print(f"Received new object with hash {message_hash}: {shared_object}")
             self.broadcast(message)
         except json.JSONDecodeError:
@@ -79,10 +83,15 @@ class ChaincraftNode:
 
     def gossip(self):
         while True:
-            if self.shared_objects:
-                object_to_share = random.choice(list(self.shared_objects.values()))
-                self.broadcast(object_to_share.to_json())
-            time.sleep(5)
+            try:
+                _ = len(self.db)
+                if len(self.db) > 0:
+                    random_key = random.choice(list(self.db.keys()))
+                    object_to_share = self.db[random_key].decode()
+                    self.broadcast(object_to_share)
+                time.sleep(5)
+            except:
+                return
 
     def create_shared_object(self, data):
         new_object = SharedObject(
@@ -91,5 +100,8 @@ class ChaincraftNode:
         )
         message = new_object.to_json()
         message_hash = self.broadcast(message)
-        self.shared_objects[message_hash] = new_object
+        self.db[message_hash] = message
         return message_hash, new_object
+
+    def close(self):
+        self.db.close()
