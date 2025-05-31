502cbdf9dda1e71ca3df54fdceb5817a16a1b307
José <30682875+jo-tm@users.noreply.github.com>
2025-05-31 17:15:49 -0300

    version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 # For backward compatibility with older pip/setuptools versions
 setup(
     name="chaincraft",
-    version="0.3.2",
+    version="0.4.1",
     packages=["chaincraft", "examples"],
     package_data={"": ["*.md", "*.txt"]},
     include_package_data=True,
